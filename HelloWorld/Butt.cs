22163c1101fe48610fcd353aa63de1dff26fee71
minTwin <michaelmin2020@gmail.com>
2023-09-05 23:00:11 -0700

    prototyping

--- HelloWorld/Butt.cs
+++ HelloWorld/Butt.cs
@@ -2,43 +2,52 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
-using Assessment; //Assessment.dll
+using System.Threading.Tasks;
+
+namespace Assessment
+{
+    public interface IWordsProvider
+    {
+        List<string> GetWordList();
+    }
+}
+
 
 namespace NetworkSecurityScanner
 {
     class Program
     {
         static readonly HttpClient client = new HttpClient();
-        static readonly string targetIpAddress = "192.168.1.10"; // Hardcoded IP address
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            Console.WriteLine("Enter target IP address:");
+            string targetIpAddress = Console.ReadLine(); // Read IP from user input
+
             Console.WriteLine("Starting scan for flag.txt...");
 
-            List<string> wordList = Words.GetWordList(); // Assuming GetWordList() returns a List<string>
+            List<string> wordList = Assessment.Words.GetWordList();
 
-            foreach (string path in wordList)
-            {
-                CheckForFlagFile(path);
-            }
+            var tasks = wordList.Select(path => CheckForFlagFile(path, targetIpAddress)).ToArray();
+            await Task.WhenAll(tasks); // Wait for all checks to complete
 
             Console.WriteLine("Scan completed.");
         }
 
-        static async void CheckForFlagFile(string path)
+        static async Task CheckForFlagFile(string path, string ipAddress)
         {
-            string targetUrl = $"http://{targetIpAddress}{path}/flag.txt"; // Use the hardcoded IP
+            string targetUrl = $"http://{ipAddress}{path}/flag.txt";
 
             try
             {
                 HttpResponseMessage response = await client.GetAsync(targetUrl);
 
-                if (response.StatusCode == HttpStatusCode.OK)
+                if (response.IsSuccessStatusCode && await response.Content.ReadAsStringAsync() != "")
                 {
                     Console.WriteLine($"flag.txt found at: {targetUrl}");
                 }
             }
-            catch (HttpRequestException e)
+            catch (Exception e) // Catch all exceptions
             {
                 Console.WriteLine($"Error fetching {targetUrl}. Message: {e.Message}");
             }
