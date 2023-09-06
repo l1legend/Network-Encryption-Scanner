8f680ad0dfa8953cb61278c92ca2e54ddf2c7db8
minTwin <michaelmin2020@gmail.com>
2023-09-05 22:43:22 -0700

    added Assessment.dll as a dependency to the project

--- HelloWorld/Butt.cs
+++ HelloWorld/Butt.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Http;
+//using Assessment; // Assuming Assessment.dll is referenced
 
-namespace HelloWorld
+namespace NetworkSecurityScanner
 {
-    internal class Butt
+    class Program
     {
+        static readonly HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting scan for flag.txt...");
+
+            List<string> wordList = Words.GetWordList(); // Assuming GetWordList() returns a List<string>
+
+            foreach (string path in wordList)
+            {
+                CheckForFlagFile(path);
+            }
+
+            Console.WriteLine("Scan completed.");
+        }
+
+        static async void CheckForFlagFile(string path)
+        {
+            string targetUrl = $"{path}/flag.txt";
+
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(targetUrl);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    Console.WriteLine($"flag.txt found at: {targetUrl}");
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Error fetching {targetUrl}. Message: {e.Message}");
+            }
+        }
     }
 }
