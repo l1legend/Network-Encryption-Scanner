b29d31046cc8f34e350b4d85c26480556d8563f0
minTwin <michaelmin2020@gmail.com>
2023-09-07 19:20:14 -0700

    program encyption text in textfile and gives user the option to encrypt their file

--- Project/LocalDirectoryScanner.cs
+++ Project/LocalDirectoryScanner.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace NetworkScanner
 {
@@ -35,6 +36,22 @@ namespace NetworkScanner
                     Console.WriteLine($"Found {filename} at {file}");
                     Console.WriteLine($"Content of {filename}:");
                     Console.WriteLine(File.ReadAllText(file));
+
+                    Console.WriteLine("Do you want to encrypt this file? (yes/no)");
+                    var userResponse = Console.ReadLine();
+                    if (userResponse?.ToLower() == "yes")
+                    {
+                        Console.WriteLine("Enter an encryption passphrase (minimum 8 characters):");
+                        var passphrase = Console.ReadLine();
+                        if (!string.IsNullOrEmpty(passphrase) && passphrase.Length >= 8)
+                        {
+                            EncryptFile(file, passphrase);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid passphrase. File was not encrypted.");
+                        }
+                    }
                 }
 
                 // Recursively search in subdirectories
@@ -50,6 +67,48 @@ namespace NetworkScanner
             catch (Exception e)
             {
                 Console.WriteLine($"Error accessing {directory}: {e.Message}");
+            }
+        }
+
+        private static void EncryptFile(string filePath, string passphrase)
+        {
+            try
+            {
+                byte[] salt = new byte[8];
+                new RNGCryptoServiceProvider().GetBytes(salt); // Random salt for key derivation
+                Rfc2898DeriveBytes derivedKey = new Rfc2898DeriveBytes(passphrase, salt, 1000);
+                byte[] key = derivedKey.GetBytes(32); // Derived 256-bit key
+
+                byte[] iv = new byte[16]; // 128 bits IV (AES block size)
+
+                byte[] encrypted;
+
+                using (Aes aesAlg = Aes.Create())
+                {
+                    aesAlg.Key = key;
+                    aesAlg.IV = iv;
+
+                    ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                    using (MemoryStream msEncrypt = new MemoryStream())
+                    {
+                        using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                        {
+                            using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                            {
+                                swEncrypt.Write(File.ReadAllText(filePath));
+                            }
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+
+                File.WriteAllBytes(filePath, encrypted);
+                Console.WriteLine($"{filePath} has been encrypted successfully.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error encrypting {filePath}. Message: {e.Message}");
             }
         }
     }
